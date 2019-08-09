3c1e69678679338cdf4e7f192efd0b203522cb1a
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-08-09 14:44:42 -0400

    initiated react app

---
+++ utub3/src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+
+export default class componentName extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+ReactDOM.render(<App/>, document.querySelector('root'));
